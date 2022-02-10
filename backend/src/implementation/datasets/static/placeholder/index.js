97d0f95361a53ac6bd274bfe59e488e382cbd89e
ErCornet <46408373+ErCornet@users.noreply.github.com>
2022-02-10 23:47:25 +0100

    Removed Unnecessary Sensors & Static Datasets

--- backend/src/implementation/datasets/static/placeholder/index.js
+++
@@ -1,9 +0,0 @@
-const GestureSet = require('../../../../framework/gestures/gesture-set').GestureSet;
-
-function loadDataset(name, datasetPath, identifier, sensorPointsNames) {
-    return new GestureSet(name);
-}
-
-module.exports = {
-    loadDataset
-};
